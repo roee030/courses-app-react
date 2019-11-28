2a4eacfdf717c30bb21f4cc16268a9af03dc4f92
roee030 <roee.angel93@gmail.com>
2019-11-28 10:32:04 +0200

    create home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+  
+import React from 'react'
+import { Link } from "react-router-dom";
+
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            Hello from Home page
+        </div>
+    )
+}
