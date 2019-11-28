9f67139644d6e4c35c598814225405383832babe
roee030 <roee.angel93@gmail.com>
2019-11-28 10:44:18 +0200

    add 2 pages to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import Home from './pages/Home';
+import Courses from './pages/Courses'
 function App() {
   return (
-    <div className="App">
-      <a href="/">Click here</a>
-    </div>
+      <div>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/courses" component={Courses}/>
+  </div>
   );
 }
 
