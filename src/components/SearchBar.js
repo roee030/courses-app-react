3cbb4e4fb54df12000aa4442004e6a10c5f6dc07
roee030 <roee.angel93@gmail.com>
2019-12-10 11:40:51 +0200

    searchbar added to home page

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,18 @@ import { MDBCol, MDBIcon } from "mdbreact";
 import "./SearchBar.css";
 const SearchPage = () => {
   return (
+    <div className="SearchBar">
     <MDBCol md="6">
-      <div className="input-group md-form form-sm form-1 pl-0">
+      <div className="input-group md-form form-sm form-1 pl-0 ">
         <div className="input-group-prepend">
-          <span className="input-group-text purple lighten-3" id="basic-text1">
+          <span className="input-group-text  lighten-3" id="basic-text1">
             <MDBIcon className="text-white" icon="search" />
           </span>
         </div>
-        <input className="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search" />
+        <input className="form-control my-0 py-1" type="text" placeholder="Search For Course" aria-label="Search" />
       </div>
     </MDBCol>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import './Home.css'
 import Courses from './Courses';
 import SearchBar from '../components/SearchBar';
+import Title from '../components/Title';
 export default function Home() {
 
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             
             
             <div className="bg"></div>
+            <Title title={"Search for Course"}/>
             <SearchBar/>
             <Courses/>
             </>
